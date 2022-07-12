35f1120806d7060718d68c0342a6bbe118821c02
Caleb Smith <minifiedcode@gmail.com>
2022-07-12 10:48:21 -0500

    Update favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,18 @@
  */
 
 import type { AppProps } from "next/app";
-import "../styles/globals.css";
+import Head from "next/head";
 import NavbarContainer from "../components/Navbar/Container";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>OneNote Project</title>
+        <link rel="shortcut icon" href="/favicon.png" />
+      </Head>
+
       <NavbarContainer />
       <Component {...pageProps} />
     </>
