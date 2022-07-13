63abc2d91ca72e7b4f436067c8bf26bc1b98421e
Jimbo Jayson <illegaljson@gmail.com>
2022-07-13 00:26:43 -0500

    Center navbar links

--- components/Navbar/Links.tsx
+++ components/Navbar/Links.tsx
@@ -6,7 +6,7 @@ import NavbarLink from "./Link";
  */
 function NavbarLinks() {
   return (
-    <div className="container mx-auto flex flex-row justify-end">
+    <div className="container mx-auto flex flex-row justify-center">
       <NavbarLink href="/" value="Home" />
       <NavbarLink href="/view-notebooks" value="View Notebooks" />
       <NavbarLink href="/view-sections" value="View Sections" />
